2c7ae13d3f3cf3e426ee7b7e44b7e80d30667f34
Chris Eggleston <chris@chriseggleston.com>
2023-11-25 19:00:41 -0700

    Create md-wpc-hide-adminbar.php

---
+++ md-wpcore/md-wpc-hide-adminbar.php
@@ -0,0 +1,9 @@
+<?php
+
+// Hide admin bar for all user roles except Administrator
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+}
