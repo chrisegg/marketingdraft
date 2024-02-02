dbf866467af6fac7ecb223f1bbd0870fc22e1347
Chris Eggleston <chris@chriseggleston.com>
2024-02-01 18:07:57 -0700

    Create md-wpc-disable-comments.php

---
+++ md-wpcore/md-wpc-disable-comments.php
@@ -0,0 +1,8 @@
+<?php 
+
+// Close comments on the front-end
+function disable_comments_status() {
+    return false;
+}
+add_filter('comments_open', 'disable_comments_status', 20, 2);
+add_filter('pings_open', 'disable_comments_status', 20, 2);
